b0eec6d3c7ccb622bee0cc023af51daa500b5a7c
Lavanyabaddala10 <lavanyabaddala10@gmail.com>
2025-06-02 23:52:58 -0930

    Add files via upload

---
+++ Stringoperationusingdynamic.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Stringoperationusingdynamic{
+
+   public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+ public class Lavanya1{
+        String a=sc.nextLine();
+        for(int i=0;i<a.length();i++)
+       {
+           System.out.println(a.charAt(i));
+       }
+}
+}
